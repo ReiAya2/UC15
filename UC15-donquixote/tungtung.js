a6aba41d90867f48ef880eefb5d568c94eccaf29
Aya <lioayshaer@gmail.com>
2026-09-17 10:29:42 -0300

    Add files via upload

---
+++ UC15-donquixote/tungtung.js
@@ -0,0 +1,10 @@
+function mudarTexto() {
+document.getElementById
+("titulo").textContent = "voce clicou no botão!";
+
+}
+
+function mudarCor() {
+document.getElementById("mensagem").style.color = "blue";
+
+}
